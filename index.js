f66f95adde81cd8e39da2d554871ff7e8ae1818c
budhaditya-itobuz <it012303@IT012303s-MacBook-Pro.local>
2024-02-16 14:49:06 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -1,91 +1,116 @@
 const input = document.getElementById('input')
 const output = document.getElementById('output')
-console.log(output.innerText)
-const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
 
-let currNum = 0
+let currNum = ""
 let result = 0
 let lastoperation = ""
 
-const reset = () => {
-    let currNum = 0
-    let result = 0
-    let lastoperation = ""
+const reset =()=>{
+    currNum = ""
+    result = 0
+    lastoperation = ""
+    input.innerText=""
+    output.innerText=""
 }
 
-
-const operation = (response) => {
+const operate = (response) => {
+    console.log(lastoperation)
     switch (response) {
         case "+":
-            result = result + currNum
-            output.innerText = result
+            result = result + parseFloat(currNum)
             break
         case "-":
-            result = result - currNum
-            output.innerText = result
+            result = result - parseFloat(currNum)
             break
         case "*":
-            result = result * currNum
-            output.innerText = result
+            result = result * parseFloat(currNum)
             break
         case "/":
-            result = result / currNum
-            output.innerText = result
+            result = result / parseFloat(currNum)
             break
         case "":
-            result = currNum
-            output.innerText = result
-            console.log(output.innerText)
+            if(currNum!="")
+            result = parseFloat(currNum)
             break
         case "√x":
-            result = Math.sqrt(currNum)
-            output.innerText = result
+            if(result>0)
+            result =result * Math.sqrt(parseFloat(currNum))
+            else
+            result=Math.sqrt(parseFloat(currNum))
             break
-
+        case "%":
+            result = result = result % currNum
+            break
     }
+    if (!isFinite(result)) 
+    {
+        reset()
+        output.innerText = "Error"
+    }
+    else if (isNaN(parseInt(result))) output.innerText = ""
+    else if (!Number.isInteger(result))
+        output.innerText = parseFloat(result.toFixed(5))
+    else
+        output.innerText = result
 }
 
 const calculate = (e) => {
+    if(output.innerText==="Error") reset()
     const response = e.innerText
-    if (num.includes(parseInt(e.innerText))) {
-        currNum = currNum * 10 + parseInt(e.innerText)
-        input.innerText = currNum
-    }
-
+if (num.includes(parseInt(e.innerText))) {
+    
+    currNum = currNum.concat(response.toString())
+    input.innerText = currNum
+    console.log(currNum)
+    return
+}
     switch (response) {
         case ".":
-            if(Number.isInteger(currNum))
-            {
-                input.innerText=currNum.toString().concat(".")
-                currNum=parseFloat()
+            if (!(currNum.indexOf('.') > -1)) {
+                currNum = currNum.concat(".")
+                input.innerText = currNum
             }
             break
         case "AC":
-            input.innerText = ""
-            output.innerText = ""
-            currNum = 0
-            result = 0
-            lastoperation = ""
+            reset()
             break
         case "C":
             if (currNum % 1 != 0) {
-                console.log('he')
                 let str = currNum.toString()
                 str = str.substring(0, str.length - 1);
                 currNum = parseFloat(str)
             }
             else
-                currNum = Math.floor(currNum / 10)
+            currNum = Math.floor(currNum / 10)
             input.innerText = currNum
             break
         case "Π":
             currNum = Math.PI
             input.innerText = Math.PI
             break
-        default:
-            input.innerText = response
-            operation(lastoperation)
+        case "√x":
+            input.innerText=input.innerText.concat("√x")
+            operate(lastoperation)
             lastoperation = response
-            currNum = 0
+            currNum = ""
+            break
+        default:
+            if(currNum==="" && lastoperation==="" && response!="√x") return
+            else if(currNum==="")
+            {
+                input.innerText = response
+                lastoperation = response
+            }
+            else
+            {
+                input.innerText = response
+                operate(lastoperation)
+                if(output.innerText==="Error")
+                return
+                lastoperation = response
+                currNum = ""
+            }
+            break
     }
 }
--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-let a=3
-
-// a=parseFloat(a).toFixed(1)
-
-a=a.toString().concat(".")
-console.log(a)
-
+let a=""
+console.log(isFinite(a))
