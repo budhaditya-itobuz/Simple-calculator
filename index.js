83eb7f888da120f2e7eca003922578fd7bae1170
budhaditya-itobuz <it012303@IT012303s-MacBook-Pro.local>
2024-02-16 15:50:34 +0530

    indentation fixed

--- index.js
+++ index.js
@@ -1,116 +1,123 @@
-const input = document.getElementById('input')
-const output = document.getElementById('output')
-const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let currNum = ""
-let result = 0
-let lastoperation = ""
+let currNum = "";
+let result = 0;
+let lastoperation = "";
 
-const reset =()=>{
-    currNum = ""
-    result = 0
-    lastoperation = ""
-    input.innerText=""
-    output.innerText=""
-}
+const reset = () => {
+    currNum = "";
+    result = 0;
+    lastoperation = "";
+    input.innerText = "";
+    output.innerText = "";
+};
 
 const operate = (response) => {
-    console.log(lastoperation)
     switch (response) {
         case "+":
-            result = result + parseFloat(currNum)
-            break
+            result = result + parseFloat(currNum);
+            break;
         case "-":
-            result = result - parseFloat(currNum)
-            break
+            result = result - parseFloat(currNum);
+            break;
         case "*":
-            result = result * parseFloat(currNum)
-            break
+            result = result * parseFloat(currNum);
+            break;
         case "/":
-            result = result / parseFloat(currNum)
-            break
+            result = result / parseFloat(currNum);
+            break;
         case "":
-            if(currNum!="")
-            result = parseFloat(currNum)
-            break
+            if (currNum !== "") result = parseFloat(currNum);
+            break;
         case "√x":
-            if(result>0)
-            result =result * Math.sqrt(parseFloat(currNum))
-            else
-            result=Math.sqrt(parseFloat(currNum))
-            break
+            if (result !== 0) result = result * Math.sqrt(parseFloat(currNum));
+            else result = Math.sqrt(parseFloat(currNum));
+            break;
+        case "In()":
+            if (result !== 0) result = result * Math.log(parseFloat(currNum));
+            else result = Math.log(parseFloat(currNum));
+            break;
         case "%":
-            result = result = result % currNum
-            break
+            result = result = result % currNum;
+            break;
     }
-    if (!isFinite(result)) 
-    {
-        reset()
-        output.innerText = "Error"
-    }
-    else if (isNaN(parseInt(result))) output.innerText = ""
+    if (!isFinite(result)) {
+        reset();
+        output.innerText = "Error";
+    } else if (isNaN(parseInt(result))) output.innerText = "";
     else if (!Number.isInteger(result))
-        output.innerText = parseFloat(result.toFixed(5))
-    else
-        output.innerText = result
-}
+        output.innerText = parseFloat(result.toFixed(5));
+    else output.innerText = result;
+};
 
 const calculate = (e) => {
-    if(output.innerText==="Error") reset()
-    const response = e.innerText
-if (num.includes(parseInt(e.innerText))) {
-    
-    currNum = currNum.concat(response.toString())
-    input.innerText = currNum
-    console.log(currNum)
-    return
-}
+    if (output.innerText === "Error") reset();
+    const response = e.innerText;
+    if (num.includes(parseInt(e.innerText))) {
+        currNum = currNum.concat(response.toString());
+        if (lastoperation === "√x") {
+            if (result !== 0) input.innerText = result + "√" + currNum;
+            else input.innerText = "√" + currNum;
+        } else if (lastoperation === "In()") {
+            if (result !== 0) input.innerText = result + "log(" + currNum + ")";
+            else input.innerText = "In(" + currNum + ")";
+        } else input.innerText = currNum;
+        return;
+    }
     switch (response) {
         case ".":
-            if (!(currNum.indexOf('.') > -1)) {
-                currNum = currNum.concat(".")
-                input.innerText = currNum
+            if (!(currNum.indexOf(".") > -1)) {
+                currNum = currNum.concat(".");
+                input.innerText = currNum;
             }
-            break
+            break;
         case "AC":
-            reset()
-            break
+            reset();
+            break;
         case "C":
-            if (currNum % 1 != 0) {
-                let str = currNum.toString()
+            if (currNum % 1 !== 0) {
+                let str = currNum.toString();
                 str = str.substring(0, str.length - 1);
-                currNum = parseFloat(str)
-            }
-            else
-            currNum = Math.floor(currNum / 10)
-            input.innerText = currNum
-            break
+                currNum = parseFloat(str);
+            } else currNum = Math.floor(currNum / 10);
+            input.innerText = currNum;
+            break;
         case "Π":
-            currNum = Math.PI
-            input.innerText = Math.PI
-            break
+            currNum = Math.PI;
+            input.innerText = Math.PI;
+            break;
         case "√x":
-            input.innerText=input.innerText.concat("√x")
-            operate(lastoperation)
-            lastoperation = response
-            currNum = ""
-            break
+            input.innerText = input.innerText.concat("√x");
+            operate(lastoperation);
+            lastoperation = response;
+            currNum = "";
+            break;
+        case "log()":
+            input.innerText = input.innerText.concat("log()");
+            operate(lastoperation);
+            lastoperation = response;
+            currNum = "";
+            break;
         default:
-            if(currNum==="" && lastoperation==="" && response!="√x") return
-            else if(currNum==="")
-            {
-                input.innerText = response
-                lastoperation = response
+            if (
+                currNum === "" &&
+                lastoperation === "" &&
+                response !== "√x" &&
+                response !== "In()"
+            )
+                return;
+            else if (currNum === "") {
+                input.innerText = response;
+                lastoperation = response;
+            } else {
+                input.innerText = response;
+                operate(lastoperation);
+                if (output.innerText === "Error") return;
+                lastoperation = response;
+                currNum = "";
             }
-            else
-            {
-                input.innerText = response
-                operate(lastoperation)
-                if(output.innerText==="Error")
-                return
-                lastoperation = response
-                currNum = ""
-            }
-            break
+            break;
     }
-}
+};
